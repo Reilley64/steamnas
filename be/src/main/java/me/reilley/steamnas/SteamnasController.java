39ad6aea4bcdc9e596ecb15b141ec164248ca497
Reilley <reilley@capitalroad.com.au>
2023-05-27 14:46:31 +1000

    Cross origin all

--- be/src/main/java/me/reilley/steamnas/SteamnasController.java
+++ be/src/main/java/me/reilley/steamnas/SteamnasController.java
@@ -3,10 +3,7 @@ package me.reilley.steamnas;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import me.reilley.steamnas.ext.steam.SteamFeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +12,7 @@ import java.util.stream.IntStream;
 @AllArgsConstructor
 
 @RestController
+@CrossOrigin(origins = "*")
 public class SteamnasController {
     private final AppRepository appRepository;
 
