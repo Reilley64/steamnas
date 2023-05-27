64022dd3f985ae29f9456f6513265c95b80ba0f9
Reilley <reilley@capitalroad.com.au>
2023-05-27 16:45:31 +1000

    Remove SockJS

--- be/src/main/java/me/reilley/steamnas/SteamnasWebSocketConfig.java
+++ be/src/main/java/me/reilley/steamnas/SteamnasWebSocketConfig.java
@@ -17,6 +17,6 @@ public class SteamnasWebSocketConfig implements WebSocketMessageBrokerConfigurer
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
     }
 }
