d67c937fa091a1f1cbb1c1f6502b53377114d67a
Reilley <reilley@capitalroad.com.au>
2023-05-27 16:17:11 +1000

    setAllowedOriginPatterns

--- be/src/main/java/me/reilley/steamnas/SteamnasWebSocketConfig.java
+++ be/src/main/java/me/reilley/steamnas/SteamnasWebSocketConfig.java
@@ -17,6 +17,6 @@ public class SteamnasWebSocketConfig implements WebSocketMessageBrokerConfigurer
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
